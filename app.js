5cf39a09ec1440ea3408c71fa4f184c69a7eaabc
Jesse <jessesouzaall@gmail.com>
2022-07-27 19:26:04 -0300

    5 corrigi os erros de console, eram erros de tipo do js. agora criei o ts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-alert("bem vindo a essa calculadora");
-console.log("estou vinculado ao index.html")
+//alert("bem vindo a essa calculadora");
+
+//console.log("estou vinculado ao index.html")
 
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
@@ -7,11 +8,17 @@ let button = document.getElementById("botao1");
 
 
 function somarNumeros(n1, n2){
-    return n1+n2
+    if(typeof n1 ===  'number' && typeof n2 ===  'number'){
+        return (n1+n2)
+    }else{
+        let result = Number(n1)+Number(n2)
+        return alert(result)
+    }
+    
 }
 
 button.addEventListener('click', () =>{
-    console.log(somarNumeros(input1, input2));
+    console.log(somarNumeros(input1.value, input2.value));
 })  
 
 
