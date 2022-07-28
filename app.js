86cf03cb68fcc365341d842f2d198d51edc5f7af
Jesse <jessesouzaall@gmail.com>
2022-07-28 00:17:45 -0300

    implementei várias operações diferentes por minha conta. fiz em js depois refiz em ts retirando os bugs e tipando, mas não consigo compilar para ts.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //alert("bem vindo a essa calculadora");
+//alert(somarNumeros(input1, input2));
 
-//console.log("estou vinculado ao index.html")
+console.log("estou vinculado ao index.html")
 
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
-let button = document.getElementById("botao1");
+let soma = document.getElementById("botao1");
+let sub = document.getElementById("botao2");
+let mult = document.getElementById("botao3");
+let div = document.getElementById("botao4");
 
 
 function somarNumeros(n1, n2){
@@ -12,14 +16,53 @@ function somarNumeros(n1, n2){
         return (n1+n2)
     }else{
         let result = Number(n1)+Number(n2)
-        return alert(result)
+        return alert('a soma é: '+ result)
     }
     
 }
 
-button.addEventListener('click', () =>{
+soma.addEventListener('click', () =>{
     console.log(somarNumeros(input1.value, input2.value));
-})  
+})
 
+function subtrairNumeros(n1, n2){
+    if(typeof n1 ===  'number' && typeof n2 ===  'number'){
+        return (n1-n2)
+    }else{
+        let result = Number(n1)-Number(n2)
+        return alert('a subtração é: '+ result)
+    }
+    
+}
 
-//alert(somarNumeros(input1, input2));
+sub.addEventListener('click', () =>{
+    console.log(subtrairNumeros(input1.value, input2.value));
+})
+
+function multiplicarNumeros(n1, n2){
+    if(typeof n1 ===  'number' && typeof n2 ===  'number'){
+        return (n1*n2)
+    }else{
+        let result = Number(n1)*Number(n2)
+        return alert('o produto é: '+ result)
+    }
+    
+}
+
+mult.addEventListener('click', () =>{
+    console.log(multiplicarNumeros(input1.value, input2.value));
+})
+
+function dividirNumeros(n1, n2){
+    if(typeof n1 ===  'number' && typeof n2 ===  'number'){
+        return (n1/n2)
+    }else{
+        let result = Number(n1)/Number(n2)
+        return alert('a divisão é: '+ result)
+    }
+    
+}
+
+div.addEventListener('click', () =>{
+    console.log(dividirNumeros(input1.value, input2.value));
+})
---
+++ app.ts
@@ -0,0 +1,17 @@
+// let input1 = document.getElementById("input1") as HTMLInputElement; //pdoeriam ser qualquer coisa, aí precisamos especificar que é um input element
+// let input2 = document.getElementById("input2") as HTMLInputElement;
+// let button3 = document.getElementById("botao1");
+
+// function somarNumeros(n1: number, n2: number){
+//     let result = n1+n2
+//     return alert(result)
+// }
+
+// if (button3){ //se esse botão existir
+//     button3.addEventListener('click', () =>{
+//         if(input1 && input2){//se esses inputs existirem
+//             somarNumeros(Number(input1.value), (Number(input2.value))) //como .value é do tipo string nós precisamos converter
+//         }
+//     })
+
+// }
